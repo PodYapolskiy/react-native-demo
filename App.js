bc94a9fb5a01da863b2329bc76b6ce9e34b77b13
Anatoly <podyapolskiyaa@gmail.com>
2021-06-30 22:56:02 +0300

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, View, ScrollView, FlatList } from 'react-native'
+import { Navbar } from './src/Navbar'
+import { AddTodo } from './src/AddTodo'
+import { Todo } from './src/Todo'
 
 export default function App() {
+  const [todos, setTodos] = useState([]) // Изменение состояния какого-либо объекта
+
+  const addTodo = title => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+
+    // setTodos(todos.concat([ newTodo]))
+    // setTodos((prevTodos) => {
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title, // title: title; когда одинаковые значения
+      },
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+
+        <FlatList // Более оптимизированная версия, прогружающая элементы только когда надо
+          keyExtractor={item => item.id.toString()} // Ключ должен быть строкой
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item} />}
+        />
+
+        {/* <ScrollView>
+          {todos.map(todo => (
+            <Todo todo={todo} key={todo.id} /> // <Text key={todo.id}>{todo.title}</Text>
+          ))}
+        </ScrollView> */}
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 20,
   },
-});
+})
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,  // Занимает всю доступную высоту экрана
+//     flexDirection: 'column',  // Выравнивания ниже зависят от этого
+//     backgroundColor: '#000',
+//     alignItems: 'flex-end',  // Выравнивание по горизонтали
+//     justifyContent: 'center',  // Выравнивание по вертикали
+//   },
+//   text: {
+//     color: '#fff',
+//     fontSize: 26
+//   }
+// });
---
+++ src/AddTodo.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { StyleSheet, TextInput, Button, View, Alert } from 'react-native'
+
+export const AddTodo = props => {
+  const [value, setValue] = useState('')
+
+  const pressHandler = () => {
+    // Если пустой инпут
+    if (value.trim()) {
+      props.onSubmit(value)
+      setValue('')
+    } else {
+      Alert.alert('Название дела не может быть пустым')
+    }
+  }
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setValue} // {text => setValue(text)}
+        value={value}
+        placeholder='Введите название дела...'
+        autoCorrect={false} // Убирает исправления Т9
+        autoCapitalize='none' // Убирает автоматическую капитализацию
+      />
+      <Button title='Добавить' onPress={pressHandler} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 15,
+  },
+  input: {
+    width: '70%',
+    padding: 10,
+    borderStyle: 'solid',
+    borderBottomWidth: 2,
+    borderBottomColor: '#3949ab',
+  },
+})
