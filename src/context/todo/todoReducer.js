6f26f253dfc3a459490654fd96fa4ce163abaa14
Anatoly <podyapolskiyaa@gmail.com>
2021-07-08 14:07:19 +0300

    47. Рефакторинг редюсера

--- src/context/todo/todoReducer.js
+++ src/context/todo/todoReducer.js
@@ -1,40 +1,39 @@
 // Название файла с маленькой буквы, потому что не JSX компонент
 import { ADD_TODO, UPDATE_TODO, REMOVE_TODO } from '../types'
 
+const handlers = {
+  // Возвращаем объекты
+  // Квадратные скобки - обращение к константе
+  // Обязательно не изменяем уже существующий оюъект, а создаём ссылку на новый
+  [ADD_TODO]: (state, { title }) => ({
+    ...state, // Разворачиваем нынешний state
+    todos: [
+      // Изменяем поле todos
+      ...state.todos,
+      {
+        id: Date.now().toString(),
+        title,
+      },
+    ],
+  }),
+  [REMOVE_TODO]: (state, { id }) => ({
+    ...state,
+    todos: state.todos.filter(todo => todo.id !== id),
+  }),
+  [UPDATE_TODO]: (state, { title, id }) => ({
+    ...state,
+    todos: state.todos.map(todo => {
+      if (todo.id === id) {
+        todo.title = title
+      }
+      return todo
+    }),
+  }),
+  DEFAULT: state => state,
+}
+
 // ? Какой-то Reducer
 export const todoReducer = (state, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      // Обязательно не изменяем уже существующий оюъект, а создаём ссылку на новый
-      return {
-        ...state, // Разворачиваем нынешний state
-        todos: [
-          // Изменяем поле todos
-          ...state.todos,
-          {
-            id: Date.now().toString(),
-            title: action.title,
-          },
-        ],
-      }
-
-    case REMOVE_TODO:
-      return {
-        ...state,
-        todos: state.todos.filter(todo => todo.id !== action.id),
-      }
-
-    case UPDATE_TODO:
-      return {
-        ...state,
-        todos: state.todos.map(todo => {
-          if (todo.id === action.id) {
-            todo.title = action.title
-          }
-          return todo
-        }),
-      }
-    default:
-      return state
-  }
+  const handler = handlers[action.type] || handlers.DEFAULT
+  return handler(state, action)
 }
